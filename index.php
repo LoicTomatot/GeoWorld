d365fed467614c33728505ef2e59700d4cdb5c9d
Alan DESMURS <desmurs.alan@gmail.com>
2020-03-25 15:54:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <?php
+// On lance une session
 session_start();
-require_once 'header.php';
+// On vérifie si une action est effectué
 if(!isset($_REQUEST['action'])) {
   $action = "accueil";
 }
 else {
   $action = $_REQUEST['action'];
 }
+// On vérifie si l'action
 switch ($action) {
   case 'accueil':
+    include('header.php');
     include("vues/accueil.php");
     break;
   case 'todo':
-    include("vues/todo-project.php");
+    include('header.php');
+    include("vues/todo-projet.php");
     break;
   case 'login':
     include("vues/login.php");
     break;
   case 'register':
     include("vues/register.php");
-    break;
-  case 'todo':
-    include("vues/todo-project.php");
     break;
   
   default:
