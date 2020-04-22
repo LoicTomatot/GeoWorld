071f25f53b7a0973e877c9a704f0c0203ce344d1
Alan DESMURS <desmurs.alan@gmail.com>
2020-04-22 18:15:51 -0400

    Update register.php

--- vues/register.php
+++ vues/register.php
@@ -8,7 +8,6 @@
     </head>
     <body>
 		<div id="container">
-	        <fieldset>     
 	        <legend>Inscription</legend>
 	        <form method="post" action="enregistrement.html">
 				<input type="text" placeholder="Votre nom" name="nom" value="<?php if(isset($nom)){ echo $nom; }?>" required><br />
@@ -16,17 +15,12 @@
 	            <input type="email" placeholder="Adresse mail" name="mail" value="<?php if(isset($mail)){ echo $mail; }?>" required><br />
 	            <input type="text" placeholder="Numéro de téléphone" name="tel" value="<?php if(isset($tel)){ echo $tel; }?>" required><br />
 	            <input type="text" placeholder="Code enseignant" name="code" value="<?php if(isset($code)){ echo $code; }?>" required><br /><br />
-	       </fieldset>
-	            
-	        <fieldset>
 	        	<legend>Authentification</legend>
 	            <input type="identifiant" placeholder="identifiant" name="identifiant" value="<?php if(isset($identifiant)){ echo $identifiant; }?>" required><br /><br />
 	            <input type="password" placeholder="Mot de passe" name="mdp" value="<?php if(isset($mdp)){ echo $mdp; }?>" required><br />
 	            <input type="password" placeholder="Confirmer le mot de passe" name="confmdp" required><br /><br />
-	       	</fieldset>
 			<input type="submit" name="inscription" value="S'incrire">
 	        </form>
-			</fieldset>
 		</div>
     </body>
 </html>
